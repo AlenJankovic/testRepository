2edcb6b98012e1b36057f7df74c5716a624fb860
AlenJankovic <alen.jankovic@bredband.net>
2019-05-04 12:13:06 +0200

    TestGit

---
+++ testAplication/testAplication-ejb/src/java/pac/NewSessionBean.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * 
+ * 
+ */
+package pac;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author alenj
+ */
+@Stateless
+@LocalBean
+public class NewSessionBean {
+     
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
